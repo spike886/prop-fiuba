9ac3b3acdc6d8ab7e528408dbd14f335969131da
Andres <spike886@gmail.com>
2014-09-06 17:38:32 -0300

    Teminados cambios visuales

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,14 @@ module Prop
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application"   : "devise" }
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"
+      Devise::PasswordsController.layout "devise"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
